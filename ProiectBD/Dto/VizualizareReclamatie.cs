d1cef5984fb3b8496a8c517915f1fbbba2998262
Florin-Alexandru Dima <87763734+dimaflorinalex@users.noreply.github.com>
2023-12-18 22:02:40 +0200

    Changed V_RECLAMATII

--- ProiectBD/Dto/VizualizareReclamatie.cs
+++ ProiectBD/Dto/VizualizareReclamatie.cs
@@ -6,13 +6,25 @@ namespace ProiectBD.Dto
     {
         public static class Columns
         {
-            public const string Id = "ID";
-            public const string IdClient = "ID_CLIENT";
+            public const string IdReclamatie = "ID_RECLAMATIE";
             public const string Informatii = "INFORMATII";
+            public const string DataInregistrare = "DATA_INREGISTRARE";
+            public const string DataSolutionare = "DATA_SOLUTIONARE";
+            public const string IdClient = "ID_CLIENT";
+            public const string Nume = "NUME";
+            public const string Email = "EMAIL";
+            public const string Telefon = "TELEFON";
+            public const string TipPersoana = "TIP_PERSOANA";
         }
 
-        required public int Id { get; set; }
-        required public int IdClient { get; set; }
+        required public int IdReclamatie { get; set; }
         required public string Informatii { get; set; }
+        required public int IdClient { get; set; }
+        required public DateTime DataInregistrare { get; set; }
+        required public DateTime? DataSolutionare { get; set; }
+        required public string Nume { get; set; }
+        required public string Email { get; set; }
+        required public string Telefon { get; set; }
+        required public TipPersoana TipPersoana { get; set; }
     }
 }
--- ProiectBD/Pages/VizualizareReclamatii/Index.cshtml.cs
+++ ProiectBD/Pages/VizualizareReclamatii/Index.cshtml.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 using Oracle.ManagedDataAccess.Client;
-using ProiectBD.Data;
 using ProiectBD.Dto;
 using ProiectBD.Enums;
 using System.Text;
@@ -41,9 +40,15 @@ namespace ProiectBD.Pages.VizualizareReclamatii
                             {
                                 Reclamatii.Add(new VizualizareReclamatie()
                                     {
-                                        Id = reader.GetInt32(reader.GetOrdinal(VizualizareReclamatie.Columns.Id)),
-                                        IdClient = reader.GetInt32(reader.GetOrdinal(VizualizareReclamatie.Columns.IdClient)),
+                                        IdReclamatie = reader.GetInt32(reader.GetOrdinal(VizualizareReclamatie.Columns.IdReclamatie)),
                                         Informatii = reader.GetString(reader.GetOrdinal(VizualizareReclamatie.Columns.Informatii)),
+                                        DataInregistrare = reader.GetDateTime(reader.GetOrdinal(VizualizareReclamatie.Columns.DataInregistrare)),
+                                        DataSolutionare = reader.IsDBNull(reader.GetOrdinal(VizualizareReclamatie.Columns.DataSolutionare)) ? null : reader.GetDateTime(reader.GetOrdinal(VizualizareReclamatie.Columns.DataSolutionare)),
+                                        IdClient = reader.GetInt32(reader.GetOrdinal(VizualizareReclamatie.Columns.IdClient)),
+                                        Nume = reader.GetString(reader.GetOrdinal(VizualizareReclamatie.Columns.Nume)),
+                                        Email = reader.GetString(reader.GetOrdinal(VizualizareReclamatie.Columns.Email)),
+                                        Telefon = reader.GetString(reader.GetOrdinal(VizualizareReclamatie.Columns.Telefon)),
+                                        TipPersoana = Enum.Parse<TipPersoana>(reader.GetString(reader.GetOrdinal(VizualizareReclamatie.Columns.TipPersoana)))
                                     }
                                 );
                             }
